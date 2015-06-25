13dc9b742de41b85790992e45c491d653de944c6
Rodney Cullen <rodcul@gmail.com>
2015-06-25 19:47:32 +0100

    Fake user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do |i|
+ User.create(username: Faker::Internet.user_name,
+              full_name: Faker::Name.name,
+              slack_username: Faker::Internet.user_name,
+              profile_picture: Faker::Avatar.image)
+end
