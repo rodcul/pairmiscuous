876720b92218354466c74906c7920b22b7a893d1
Daryl Watson <darylwtsn@gmail.com>
2015-06-25 22:15:34 +0100

    Assigns pairs sequentially. Refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,37 @@ require 'faker'
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-c = Cohort.create(name: '#april2015')
+Cohort.create(name: '#april2015')
+Cohort.create(name: '#june2015')
+Cohort.create(name: '#alumni')
+Cohort.create(name: '#coaches')
 
-20.times do |i|
- User.create(username: Faker::Internet.user_name,
-              full_name: Faker::Name.name,
-              slack_username: Faker::Internet.user_name,
-              profile_picture: Faker::Avatar.image,
-              cohort_id: c.id)
+def assign_cohort(cohort_name, size)
+  c = Cohort.find_by(name: cohort_name)
+  size.times do |i|
+    User.create(username: Faker::Internet.user_name,
+                  full_name: Faker::Name.name,
+                  slack_username: Faker::Internet.user_name,
+                  profile_picture: Faker::Avatar.image,
+                  cohort_id: c.id)
+  end
 end
 
-c = Cohort.create(name: '#june2015')
-30.times do |i|
- User.create(username: Faker::Internet.user_name,
-              full_name: Faker::Name.name,
-              slack_username: Faker::Internet.user_name,
-              profile_picture: Faker::Avatar.image,
-              cohort_id: c.id)
+assign_cohort('#april2015', 20)
+assign_cohort('#june2015', 30)
+assign_cohort('#alumni', 10)
+assign_cohort('#coaches', 10)
+
+def assign_pairs(cohort_name)
+  cohort = Cohort.find_by(name: cohort_name)
+  x = 1
+  count = 1
+  cohort.users.each do |u|
+    Pairing.create(pair_id: x, user_id: u.id)
+    x += 1 if count.even?
+    count += 1
+  end
+
 end
-c = Cohort.create(name: '#alumni')
-10.times do |i|
- User.create(username: Faker::Internet.user_name,
-              full_name: Faker::Name.name,
-              slack_username: Faker::Internet.user_name,
-              profile_picture: Faker::Avatar.image,
-              cohort_id: c.id)
-end
-c = Cohort.create(name: '#coaches')
-10.times do |i|
- User.create(username: Faker::Internet.user_name,
-              full_name: Faker::Name.name,
-              slack_username: Faker::Internet.user_name,
-              profile_picture: Faker::Avatar.image,
-              cohort_id: c.id)
-end
+
+assign_pairs('#april2015')
