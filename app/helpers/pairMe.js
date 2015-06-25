d98263966e2524ed04c684311c3b085319e58a8a
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-25 14:05:20 +0100

    empty function

---
+++ app/helpers/pairMe.js
@@ -0,0 +1,7 @@
+
+function PairMe(userPrimaryId, userCohort, pairHistory){
+  // where userPrimaryId is a single user id asking to be paired
+  // where userCohort is an array of user id for all cohort members inc primary!
+  // where pairHistory is an array of arrays pairID, user id, date for cohort
+  
+};
