712b2d429b52b9765bb648cde2a8fe886fee385c
Rodney Cullen <rodcul@gmail.com>
2015-06-25 15:39:47 +0100

    Removed chrome from build

--- tests/karma/karma.conf.js
+++ tests/karma/karma.conf.js
@@ -56,7 +56,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome'],
+    // browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
