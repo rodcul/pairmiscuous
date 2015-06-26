ae2a328c2d60e736ac675d3cf344fc7209921f6e
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-06-26 11:48:45 +0100

    Adds a new test

--- spec/feature/user_feature_spec.rb
+++ spec/feature/user_feature_spec.rb
@@ -1,15 +1,32 @@
 require 'rails_helper'
 
+
 feature 'As a Pairmiscuous user' do
 
   before(:each) do
     User.create(username: "Molly")
     User.create(username: "Rocco")
     User.create(username: "Tim")
+    User.create(username: 'Chidu')
   end
 
   scenario 'pair is shown' do
     visit users_path
     expect(page).to have_content("You are paired with Molly");
+  end
+
+  scenario 'accepting a pair' do
+    sign_up('Rocco', 'rocco')
+    click_link('Back')
+    click_button('Accept')
+    expect(page).to have_content('Rocco and Chidu')
+    expect(page).to have_content('Congratulations on pairing.')
+  end
+
+  def sign_up(username, name)
+    visit new_user_path
+    fill_in('Full name', with: name)
+    fill_in('Username', with: username)
+    click_button('Create User')
   end
 end
