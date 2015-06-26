ed43ad1cc7bed271ba6378c5982e70a9bad843ce
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-26 12:00:08 +0100

    added test helper for setup

--- spec/pair_selection_spec.rb
+++ spec/pair_selection_spec.rb
@@ -4,25 +4,28 @@ include PairingsHelper
 
 describe 'Create a pair' do
 
-  it 'should return an available pair partner' do
-
-    cohort = Cohort.create()
-    user = []
-    user[1] = cohort.users.create()
-    user[2] = cohort.users.create()
-
-    result = pair_me(user[1],cohort)
-    expect(result).to eq([user[2]])
+  def create_test_cohort(cohort_size)
+    @cohort = Cohort.create()
+    @user = []
+    cohort_size.times {@user << @cohort.users.create()}
   end
 
-  it 'should return all available partners excluding the user being matched' do
-    cohort = Cohort.create()
-    user = []
-    20.times {user << cohort.users.create()}
-    result = pair_me(user[0],cohort).sort
+  it 'should return a cohort pair partner' do
+    create_test_cohort(2)
+    result = pair_me(@user[0],@cohort)
+    expect(result).to eq([@user[1]])
+  end
+
+  it 'should return all cohort partners excluding the user being matched' do
+    create_test_cohort(20)
+    result = pair_me(@user[0],@cohort).sort
     expect(result.length).to eq(19)
-    user[1..19].each { |use| expect(result).to include(use) }
-    expect(result).not_to include(user[0])
+    @user[1..19].each { |use| expect(result).to include(use) }
+    expect(result).not_to include(@user[0])
+  end
+
+  it 'should return all cohort partners excluding those already paired today' do
+
   end
 
 end
